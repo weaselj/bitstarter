00aecf5318ef58ac74078151e8a7bbf1e496ddaf
Shahar Jamshy <shahar@Shahar-MBP.local>
2013-07-05 21:43:49 +0300

    grader with url query

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -36,6 +37,11 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var parseUrl = function(infile) {
+    var instr = infile.toString();
+    return instr;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -44,25 +50,64 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(cheeriohtml, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
+        var present = cheeriohtml(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
 };
 
-if(require.main == module) {
-    program
-        .option('-c, --checks ', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
-        .option('-f, --file ', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+var checkHtmlFile = function(htmlfile, checksfile) {
+    cheeriohtml = cheerioHtmlFile(htmlfile);
+    return checkHtml(cheeriohtml, checksfile);
+};
+
+var logConsole = function(checkJson) {
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+};
+
+var checkHtmlFileAndLog = function(htmlfile, checksfile) {
+    var checkJson = checkHtmlFile(htmlfile, checksfile);
+    logConsole(checkJson);
+};
+
+var builfHtmlUrlResponse = function(checksfile) {
+    var responseFunction = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            var cheerioHtmlUrl = cheerio.load(result);
+            var checkJson = checkHtml(cheerioHtmlUrl, checksfile);
+            logConsole(checkJson);
+        }
+    };
+    return responseFunction;
+};
+
+var checkHtmlUrlAndLog = function(htmlurl, checksfile) {
+    var responseFunction = builfHtmlUrlResponse(checksfile);
+    rest.get(htmlurl).on('complete', responseFunction);
+
+}
+
+if(require.main == module) {
+    program
+        .option('-c, --checks [checks_file]', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+        .option('-f, --file [html_file]', 'Path to index.html', assertFileExists)
+        .option('-u, --url <html_url>', 'Url to index.html', parseUrl)
+        .parse(process.argv);
+    console.error(program.file + ' ' + program.checks + ' ' + program.url)
+    if (program.file) {
+        checkHtmlFileAndLog(program.file, program.checks);
+    } else if (program.url) {
+        checkHtmlUrlAndLog(program.url, program.checks);
+    } else {
+        checkHtmlFileAndLog(HTMLFILE_DEFAULT, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
